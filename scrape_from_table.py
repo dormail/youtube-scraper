0844d0610737a013098c29f7efbb63f731884879
Matthias Maile <matthias.maile@udo.edu>
2021-02-16 21:48:12 +0100

    Switched to ods file format since xlsx isnt supported

--- scrape_from_table.py
+++ scrape_from_table.py
@@ -1,25 +1,27 @@
 # scrape_from_table.py
 # scrape_from_table gets receives a dataframe and scrapes 
 # the youtube vid info for every link in the 'link' column
+# returns a list with a dict for every video given in the df
 
 from get_video_data import get_video_data
+from links_to_id import links_to_id
 import pandas as pd
 import numpy as np
 
 gvd = np.vectorize(get_video_data)
+ldi = np.vectorize(links_to_id)
 
 def scrape_from_table(df):
     links = df['link']
-    vid_id = split('watch?v=', links)
-    vid_info = gvd(links)
+    vid_id = ldi(links)
+    vid_info = gvd(vid_id)
 
     return vid_info
 
 
 # module test
 if __name__ == '__main__':
-    filename = 'test.xlsx'
+    filename = 'test.ods'
     df = pd.read_excel(filename)
-
-    links = scrape_from_table(df)
-    print(links)
+    info = scrape_from_table(df)
+    print(info)
