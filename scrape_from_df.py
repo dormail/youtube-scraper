2306ef2999ebf877aa0b49eedc3fd5ceae6a0523
Matthias Maile <matthias.maile@udo.edu>
2021-02-17 14:33:55 +0100

    Refactor for using for using df instead of table

--- scrape_from_df.py
+++ scrape_from_df.py
@@ -1,5 +1,5 @@
-# scrape_from_table.py
-# scrape_from_table gets receives a dataframe and scrapes 
+# scrape_from_df.py
+# scrape_from_df receives a dataframe and scrapes
 # the youtube vid info for every link in the 'link' column
 # returns a list with a dict for every video given in the df
 
@@ -11,8 +11,9 @@ import numpy as np
 gvd = np.vectorize(get_video_data)
 ldi = np.vectorize(links_to_id)
 
-def scrape_from_table(df):
-    links = df['link']
+
+def scrape_from_df(dataframe):
+    links = dataframe['link']
     vid_id = ldi(links)
     vid_info = gvd(vid_id)
 
@@ -23,5 +24,5 @@ def scrape_from_table(df):
 if __name__ == '__main__':
     filename = 'test.ods'
     df = pd.read_excel(filename)
-    info = scrape_from_table(df)
+    info = scrape_from_df(df)
     print(info)
